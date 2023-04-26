bf0a49f933833b7c2eca0cc4fec9a8692ca105ac
schigul1 <schigul1@binghamton.edu>
2023-04-26 04:14:28 -0400

    Fix displaying the uploaded videos

--- src/app/Dashboard.js
+++ src/app/Dashboard.js
@@ -15,33 +15,47 @@ export default function Dashboard() {
   useEffect(() => {
     if (currentUser) {
       const db = fbase.db;
-      const unsubscribeDB = db.collection('videolinks')
-        .where('userId', '==', currentUser.uid)
+      const unsubscribeDB = db.collection('users').doc(currentUser.uid).collection('videolinks')
         .onSnapshot((snapshot) => {
           const videos = snapshot.docs.map((doc) => ({
             id: doc.id,
             url: doc.data().url,
           }));
           setVideoLinks(videos);
+          console.log(videos)
         });
       return () => unsubscribeDB();
     }
   }, [currentUser]);
 
-  const handlePlay = (url) => {
-    const video = document.createElement('video');
-    video.src = url;
-    video.controls = true;
-    video.play();
-  };
+  // const handlePlay = (url) => {
+  //   const video = document.createElement('video');
+  //   video.src = url;
+  //   video.controls = true;
+  //   video.play();
+  //   document.getElementById('video-container').appendChild(video);
+  //
+  // };
+
+  useEffect(() => {
+    const videoContainer = document.getElementById('video-container');
+    videoLinks.forEach((video) => {
+      const videoElement = document.createElement('video');
+      videoElement.src = video.url;
+      videoElement.controls = true;
+      videoContainer.appendChild(videoElement);
+    });
+  }, [videoLinks]);
 
   return (
     <div>
-      {videoLinks.map((video) => (
-        <div key={video.id}>
-          <button onClick={() => handlePlay(video.url)}>Play Video</button>
-        </div>
-      ))}
+      <div id="video-container"></div>
+
+      {/*{videoLinks.map((video) => (*/}
+      {/*  <div key={video.id}>*/}
+      {/*    <button onClick={() => handlePlay(video.url)}>Play Video</button>*/}
+      {/*  </div>*/}
+      {/*))}*/}
     </div>
   );
 }
