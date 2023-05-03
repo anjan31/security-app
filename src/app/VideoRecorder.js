bd139dc13a1028315a48284c78e204a385235399
sathwiq80 <sathwiq.chigullarevu@copart.com>
2023-05-03 18:39:42 -0400

    UI fixes

--- src/app/VideoRecorder.js
+++ src/app/VideoRecorder.js
@@ -1,5 +1,6 @@
 import React, { useRef, useState, useEffect } from 'react';
 import fbase from "../config/Firebase";
+import './videorecorder.css';
 import { FaPlay, FaStop, FaCamera, FaRedo } from 'react-icons/fa';
 
 export default function VideoRecorder() {
@@ -101,24 +102,23 @@ export default function VideoRecorder() {
 
   return (
     <div className="video-recorder-container">
-      <div className="video-preview-container">
+      <div className="container-recorder">
         {videoPreviewUrl ? (
           <video src={videoPreviewUrl} autoPlay controls />
         ) : (
-          <video ref={videoRef} autoPlay
-          className="video-preview"
+          <video ref={videoRef} autoPlay className="video-preview"
           />
           )}
         </div>
         <div className="video-controls">
           {!isRecording && !videoPreviewUrl && (
             <button onClick={handleStartRecording} disabled={!cameraStream || isLoading}>
-              <FaPlay />
+              Start
             </button>
           )}
           {isRecording && (
             <button onClick={handleStopRecording}>
-              <FaStop />
+              Stop
             </button>
           )}
           {recordedChunks.length > 0 && (
@@ -128,11 +128,11 @@ export default function VideoRecorder() {
           )}
           {videoPreviewUrl && (
             <button onClick={handleRetakeVideo}>
-              <FaRedo />
+              Redo
             </button>
           )}
           <button onClick={handleSwitchCamera}>
-            <FaCamera />
+            Switch Camera
           </button>
         </div>
       </div>
