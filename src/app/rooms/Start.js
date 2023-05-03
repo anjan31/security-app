682c6a0a10b84298080c9dd289c6173b0569ea7b
sathwiq80 <sathwiq.chigullarevu@copart.com>
2023-05-03 13:51:14 -0400

    UI fixes

--- src/app/rooms/Start.js
+++ src/app/rooms/Start.js
@@ -5,11 +5,14 @@ import fbase from "../../config/Firebase";
 function StartButton({ onStart }) {
 
     const [currentUser, setCurrentUser] = useState(null);
+    const [roomId, setRoomId] = useState(null);
 
     useEffect(() => {
         const unsubscribeAuth = fbase.auth.onAuthStateChanged((user) => {
             setCurrentUser(user);
         });
+        window.openUserMedia();
+
         return () => unsubscribeAuth();
     }, []);
 
@@ -17,18 +20,26 @@ function StartButton({ onStart }) {
     const createRoom = async ()=>{
         await window.createRoom();
         fbase.db.collection("roomDetails").doc(window.roomId).set({roomName: 'Drive',uid: currentUser.uid})
+        setRoomId(window.roomId)
     }
 
   return (
     <div className="start-button-container">
 
-      <button className="mdc-button" onClick={window.openUserMedia}>
-            <i className="material-icons mdc-button__icon" aria-hidden="true">perm_camera_mic</i>
-            <span className="mdc-button__label">Give Access</span>
-            </button>
+      {/*<button className="mdc-button" onClick={window.openUserMedia}>*/}
+      {/*      <i className="material-icons mdc-button__icon" aria-hidden="true">perm_camera_mic</i>*/}
+      {/*      <span className="mdc-button__label">Give Access</span>*/}
+      {/*      </button>*/}
 
-      <button className="mdc-button "  onClick={createRoom}>Start</button>
-      <button className="mdc-button-danger" onClick={window.hangUp}>Hangup</button>
+        {
+            roomId?
+                (<button className="mdc-button-danger" onClick={()=>{
+                    window.hangUp();
+                    setRoomId(null);
+                }}>Hangup</button>)
+                :(<button className="mdc-button "  onClick={createRoom}>Start</button>)
+        }
+
     </div>
   );
 }
