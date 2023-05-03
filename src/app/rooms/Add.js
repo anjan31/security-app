31df9f864712ad892682c248ba0b4eca3e983e97
sathwiq80 <sathwiq.chigullarevu@copart.com>
2023-05-03 05:38:13 -0400

    UI fixes

--- src/app/rooms/Add.js
+++ src/app/rooms/Add.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Start from './Start';
+import './Add.css';
 
 function Add() {
   return (
-    <div id="videos">
+    <div id="videos" className="container">
         <video id="localVideo" muted autoPlay playsInline></video>
         <video id="remoteVideo" muted autoPlay playsInline className="displayNone"></video>
       <Start/>
